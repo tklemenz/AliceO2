54844d670a8879a0377dda2cff63731c255754f4
Thomas Klemenz <thomas.klemenz@tum.de>
2018-11-15 13:34:04 +0100

    added 20 time bin hard cut on triggered simulation

--- Detectors/TPC/base/src/ParameterDetector.cxx
+++ Detectors/TPC/base/src/ParameterDetector.cxx
@@ -16,4 +16,4 @@
 
 using namespace o2::TPC;
 
-ParameterDetector::ParameterDetector() : mTPClength(250.f), mPadCapacitance(0.1f), mTmaxTriggered(550) {}
+ParameterDetector::ParameterDetector() : mTPClength(250.f), mPadCapacitance(0.1f), mTmaxTriggered(20) {}
